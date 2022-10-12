4ae5906243ed7d1573ba9588ab9baaf8d3681270
Tony Mak <tonymak.kw@gmail.com>
2022-10-12 16:36:51 +0800

    Init template

---
+++ CICDDemo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CICDDemo
+//
+//  Created by TonyMak KW on 23/9/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
